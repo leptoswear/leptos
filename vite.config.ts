a32968ca5a2cb5102badf01ecf59fede91ed6e14
leptoswear <leptoswear@gmail.com>
2025-07-08 10:17:48 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/', // 👈 Adicione isso se estiver no domínio principal (ex: leptoswear.com.br)
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
