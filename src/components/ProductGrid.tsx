52acc4f31403912e69ec47db1ee9c5401a5b8051
leptoswear <leptoswear@gmail.com>
2025-08-01 17:06:56 -0300

    Updated ProductGrid.tsx

--- src/components/ProductGrid.tsx
+++ src/components/ProductGrid.tsx
@@ -275,18 +275,18 @@ const ProductGrid: React.FC<ProductGridProps> = ({ category, onProductClick }) =
       image: "https://i.postimg.cc/ZnnYTYPF/Whats-App-Image-2025-08-01-at-3-05-37-PM.jpg",
       category: 'camiseta',
       isNew: true,
-      colors: ['branca'],
+      colors: ['Branca'],
       sizes: ['M', 'G', 'GG'],
       description: 'Espírito rebelde em cada fio. Para quem não segue regras e cria seu próprio estilo.'
     },
     {
       id: 12,
-      name: "Camiseta Good Fellas Carro Rosa ll",
+      name: "Camiseta GodFather 50 anos Clássico",
       price: 99.90,
-      image: "https://i.postimg.cc/j5nkKHVx/Whats-App-Image-2025-08-01-at-3-05-28-PM.jpg",
+      image: "https://i.postimg.cc/zf31b2H8/Whats-App-Image-2025-08-01-at-3-05-39-PM.jpg",
       category: 'camiseta',
       isNew: true,
-      colors: ['Preta'],
+      colors: ['Branca'],
       sizes: ['M', 'G', 'GG'],
       description: 'Espírito rebelde em cada fio. Para quem não segue regras e cria seu próprio estilo.'
     },
