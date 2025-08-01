11369dbc3bfad89e43e721c45efa03edb0749805
leptoswear <leptoswear@gmail.com>
2025-08-01 18:33:56 -0300

    Updated ProductGrid.tsx

--- src/components/ProductGrid.tsx
+++ src/components/ProductGrid.tsx
@@ -632,10 +632,10 @@ const ProductGrid: React.FC<ProductGridProps> = ({ category, onProductClick }) =
       description: 'Espírito rebelde em cada fio. Para quem não segue regras e cria seu próprio estilo.'
     },
     {
-      id: 30,
-      name: "Camiseta GodFather Al Pacino",
+      id: 57,
+      name: "Camiseta Fred X Jason Fight",
       price: 99.90,
-      image: "https://i.postimg.cc/QxVPqKxJ/Whats-App-Image-2025-08-01-at-3-05-42-PM.jpg",
+      image: "https://i.postimg.cc/Vkms7c5R/Whats-App-Image-2025-08-01-at-3-07-04-PM.jpg",
       category: 'camiseta',
       isNew: true,
       colors: ['Branca'],
