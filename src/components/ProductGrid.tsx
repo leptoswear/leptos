cbbe6b49420fd4164fd3a0502c6211dac88d68fc
leptoswear <leptoswear@gmail.com>
2025-08-01 18:22:35 -0300

    Updated ProductGrid.tsx

--- src/components/ProductGrid.tsx
+++ src/components/ProductGrid.tsx
@@ -478,10 +478,10 @@ const ProductGrid: React.FC<ProductGridProps> = ({ category, onProductClick }) =
       description: 'Espírito rebelde em cada fio. Para quem não segue regras e cria seu próprio estilo.'
     },
     {
-      id: 30,
-      name: "Camiseta GodFather Al Pacino",
+      id: 44,
+      name: "Camiseta Monalisa Supreme ",
       price: 99.90,
-      image: "https://i.postimg.cc/QxVPqKxJ/Whats-App-Image-2025-08-01-at-3-05-42-PM.jpg",
+      image: "https://i.postimg.cc/kGsKBvH3/Whats-App-Image-2025-08-01-at-3-05-55-PM.jpg",
       category: 'camiseta',
       isNew: true,
       colors: ['Branca'],
