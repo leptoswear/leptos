6b2218ba07c0d56265dc7193cedb9d029d669ecc
leptoswear <leptoswear@gmail.com>
2025-08-01 18:25:12 -0300

    Updated ProductGrid.tsx

--- src/components/ProductGrid.tsx
+++ src/components/ProductGrid.tsx
@@ -511,10 +511,10 @@ const ProductGrid: React.FC<ProductGridProps> = ({ category, onProductClick }) =
       description: 'Espírito rebelde em cada fio. Para quem não segue regras e cria seu próprio estilo.'
     },
     {
-      id: 30,
-      name: "Camiseta GodFather Al Pacino",
+      id: 47,
+      name: "Camiseta Escorpião Dinheiro Venenoso",
       price: 99.90,
-      image: "https://i.postimg.cc/QxVPqKxJ/Whats-App-Image-2025-08-01-at-3-05-42-PM.jpg",
+      image: "https://i.postimg.cc/VNPvPb7j/Whats-App-Image-2025-08-01-at-3-05-58-PM.jpg",
       category: 'camiseta',
       isNew: true,
       colors: ['Branca'],
