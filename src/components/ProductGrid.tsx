d8eeedbd2df6255e9183196eea6e7010ac81706b
leptoswear <leptoswear@gmail.com>
2025-08-01 16:48:03 -0300

    Updated ProductGrid.tsx

--- src/components/ProductGrid.tsx
+++ src/components/ProductGrid.tsx
@@ -214,10 +214,10 @@ const ProductGrid: React.FC<ProductGridProps> = ({ category, onProductClick }) =
       description: 'Espírito rebelde em cada fio. Para quem não segue regras e cria seu próprio estilo.'
     },
     {
-      id: 12,
-      name: "Camiseta Good Fellas Carro Rosa ll",
+      id: 20,
+      name: "Camiseta Good Fellas Capa de Jornal",
       price: 99.90,
-      image: "https://i.postimg.cc/j5nkKHVx/Whats-App-Image-2025-08-01-at-3-05-28-PM.jpg",
+      image: "https://i.postimg.cc/pdmthnN7/Whats-App-Image-2025-08-01-at-3-05-34-PM.jpg",
       category: 'camiseta',
       isNew: true,
       colors: ['Preta'],
