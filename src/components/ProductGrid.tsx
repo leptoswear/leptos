f3a2161a95b336288c6fa410310756bfa433ce3a
leptoswear <leptoswear@gmail.com>
2025-08-01 18:10:48 -0300

    Updated ProductGrid.tsx

--- src/components/ProductGrid.tsx
+++ src/components/ProductGrid.tsx
@@ -357,10 +357,10 @@ const ProductGrid: React.FC<ProductGridProps> = ({ category, onProductClick }) =
       description: 'Espírito rebelde em cada fio. Para quem não segue regras e cria seu próprio estilo.'
     },
     {
-      id: 30,
-      name: "Camiseta GodFather Al Pacino",
+      id: 33,
+      name: "Camiseta Scarface Red",
       price: 99.90,
-      image: "https://i.postimg.cc/QxVPqKxJ/Whats-App-Image-2025-08-01-at-3-05-42-PM.jpg",
+      image: "https://i.postimg.cc/pTPHqVpY/Whats-App-Image-2025-08-01-at-3-05-45-PM.jpg",
       category: 'camiseta',
       isNew: true,
       colors: ['Branca'],
