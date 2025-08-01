02bf2658417b0f5c2a1c61b27ebca6447bce5cb6
leptoswear <leptoswear@gmail.com>
2025-08-01 18:34:41 -0300

    Updated ProductGrid.tsx

--- src/components/ProductGrid.tsx
+++ src/components/ProductGrid.tsx
@@ -643,13 +643,13 @@ const ProductGrid: React.FC<ProductGridProps> = ({ category, onProductClick }) =
       description: 'Espírito rebelde em cada fio. Para quem não segue regras e cria seu próprio estilo.'
     },
     {
-      id: 30,
-      name: "Camiseta GodFather Al Pacino",
+      id: 58,
+      name: "Camiseta Scarface Dólar",
       price: 99.90,
-      image: "https://i.postimg.cc/QxVPqKxJ/Whats-App-Image-2025-08-01-at-3-05-42-PM.jpg",
+      image: "https://i.postimg.cc/5y813PVT/Whats-App-Image-2025-08-01-at-3-07-36-PM.jpg",
       category: 'camiseta',
       isNew: true,
-      colors: ['Branca'],
+      colors: ['Preta'],
       sizes: ['M', 'G', 'GG'],
       description: 'Espírito rebelde em cada fio. Para quem não segue regras e cria seu próprio estilo.'
     },
