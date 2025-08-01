d096fa8d23bf3d346d04b409ff5490c6a3959e87
leptoswear <leptoswear@gmail.com>
2025-08-01 16:50:20 -0300

    Updated ProductGrid.tsx

--- src/components/ProductGrid.tsx
+++ src/components/ProductGrid.tsx
@@ -231,7 +231,7 @@ const ProductGrid: React.FC<ProductGridProps> = ({ category, onProductClick }) =
       image: "https://i.postimg.cc/5y3rkxdL/Whats-App-Image-2025-08-01-at-3-05-35-PM.jpg",
       category: 'camiseta',
       isNew: true,
-      colors: ['Preta'],
+      colors: ['Branca'],
       sizes: ['M', 'G', 'GG'],
       description: 'Espírito rebelde em cada fio. Para quem não segue regras e cria seu próprio estilo.'
     },
