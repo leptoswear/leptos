c9a1455b5389a5cf47394b718727fa21fabf82b6
leptoswear <leptoswear@gmail.com>
2025-08-01 18:29:19 -0300

    Updated ProductGrid.tsx

--- src/components/ProductGrid.tsx
+++ src/components/ProductGrid.tsx
@@ -566,10 +566,10 @@ const ProductGrid: React.FC<ProductGridProps> = ({ category, onProductClick }) =
       description: 'Espírito rebelde em cada fio. Para quem não segue regras e cria seu próprio estilo.'
     },
     {
-      id: 30,
-      name: "Camiseta GodFather Al Pacino",
+      id: 52,
+      name: "Camiseta Meduza Green ",
       price: 99.90,
-      image: "https://i.postimg.cc/QxVPqKxJ/Whats-App-Image-2025-08-01-at-3-05-42-PM.jpg",
+      image: "https://i.postimg.cc/j5dsR6yJ/Whats-App-Image-2025-08-01-at-3-06-03-PM.jpg",
       category: 'camiseta',
       isNew: true,
       colors: ['Branca'],
