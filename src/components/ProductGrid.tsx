19b9152a94525b6aa5559297bfda4a69c0b0eacd
leptoswear <leptoswear@gmail.com>
2025-08-13 11:43:17 -0300

    Updated ProductGrid.tsx

--- src/components/ProductGrid.tsx
+++ src/components/ProductGrid.tsx
@@ -746,10 +746,32 @@ const ProductGrid: React.FC<ProductGridProps> = ({ category, onProductClick }) =
       description: 'Espírito rebelde em cada fio. Para quem não segue regras e cria seu próprio estilo.'
     },
     {
-      id: 62,
-      name: "Camiseta Pink Panther ",
+      id: 66,
+      name: "Camiseta Putin X Jinping ",
       price: 99.90,
-      image: "https://i.postimg.cc/rs22KSrJ/Whats-App-Image-2025-08-01-at-3-09-15-PM.jpg",
+      image: "https://i.postimg.cc/cH860WjL/Whats-App-Image-2025-08-13-at-11-36-21-AM.jpg",
+      category: 'camiseta',
+      isNew: true,
+      colors: ['Branca'],
+      sizes: ['M', 'G', 'GG'],
+      description: 'Espírito rebelde em cada fio. Para quem não segue regras e cria seu próprio estilo.'
+    },
+    {
+      id: 67,
+      name: "Camiseta Freira Fuck You ll ",
+      price: 99.90,
+      image: "https://i.postimg.cc/cH860WjL/Whats-App-Image-2025-08-13-at-11-36-21-AM.jpg",
+      category: 'camiseta',
+      isNew: true,
+      colors: ['Branca'],
+      sizes: ['M', 'G', 'GG'],
+      description: 'Espírito rebelde em cada fio. Para quem não segue regras e cria seu próprio estilo.'
+    },
+    {
+      id: 66,
+      name: "Camiseta Putin X Jinping ",
+      price: 99.90,
+      image: "https://i.postimg.cc/cH860WjL/Whats-App-Image-2025-08-13-at-11-36-21-AM.jpg",
       category: 'camiseta',
       isNew: true,
       colors: ['Branca'],
